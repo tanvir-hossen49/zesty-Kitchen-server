30431e3bd77d3c0faa66c92f9132b19fa9c25523
tanvir-hossen49 <thossen49@gmail.com>
2023-05-03 00:59:02 +0600

    create route for passing  chef data

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const chefData = require("./data/chefData.json");
+
 app.get("/", (req, res) => {
   res.send("server is running");
 });
 
-
+app.get("/chef-data", (req, res) => {
+  res.send(chefData);
+});
 
 app.listen(PORT, () => {
   console.log(`dragon is runnig on port`, PORT);
