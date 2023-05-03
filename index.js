e079df84624f182fa1674e338861fe94ce39fc56
tanvir-hossen49 <thossen49@gmail.com>
2023-05-03 12:51:38 +0600

    added core

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+app.use(cors());
 
 const chefData = require("./data/chefData.json");
-
 app.get("/", (req, res) => {
   res.send("server is running");
 });
