d669816ad80624fd4e13fcec03dc849bd53a49fc
tanvir-hossen49 <thossen49@gmail.com>
2023-05-03 12:53:54 +0600

    change start script

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
+
 app.use(cors());
 
 const chefData = require("./data/chefData.json");
