3d8085dc157a392bbbb4ef15424db6c7df455068
tanvir-hossen49 <thossen49@gmail.com>
2023-05-04 16:46:48 +0600

    added popular recipes json file

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ const cors = require("cors");
 app.use(cors());
 
 const chefData = require("./data/chefData.json");
+const popularRecipes = require("./data/popularRecipes.json");
+
 app.get("/", (req, res) => {
   res.send("server is running");
 });
 
 app.get("/chef-data", (req, res) => {
   res.send(chefData);
+});
+
+app.get("/popular-recipes", (req, res) => {
+  res.send(popularRecipes);
 });
 
 app.get("/chef-data/:id", (req, res) => {
